aed6ff9161a749a6c624a81022b1d29d9cab5129
xiafu16 <lovecanele16@gmail.com>
2025-11-17 09:37:26 +0800

    add busuanzi
    function: visitor statistics

---
+++ docs/.vitepress/theme/components/MyLayout.vue
@@ -0,0 +1,28 @@
+<!-- .vitepress/theme/MyLayout.vue -->
+
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+
+// 组件1、2、3
+import BSZ from "./BuSuanZi.vue";
+</script>
+
+<template>
+  <DefaultTheme.Layout v-bind="$attrs">
+
+    <!-- doc-footer-before插槽 -->
+    <!-- <template #doc-footer-before></template> -->
+
+    <!-- layout-top插槽 -->
+    <!-- <template #layout-top></template> -->
+
+    <!-- layout-bottom插槽 -->
+    <template #layout-bottom>
+      <BSZ />
+    </template>
+
+  </DefaultTheme.Layout>
+</template>
+
+<style scoped>
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -6,21 +6,28 @@ import HomeUnderline from "./components/HomeUnderline.vue"
 import './custom.css'
 import './style/index.css'
 import type { Theme } from 'vitepress'
+import MyLayout from './components/MyLayout.vue' // 导入布局组件
+
+// 不蒜子
+import bsz from "./components/BuSuanZi.vue"
+import { inBrowser } from 'vitepress'
+import busuanzi from 'busuanzi.pure.js'
 
 
 
 const theme: Theme = {
   ...DefaultTheme,
+  Layout: MyLayout, // 应用布局组件
   enhanceApp({ app, router }) {
     app.component('GlobalAudio', GlobalAudio)
     app.component('AudioPlayer', AudioPlayer)
     app.component('HomeUnderline' , HomeUnderline)
     //busuanzi
-    // if (inBrowser) {
-    //   router.onAfterRouteChange = () => {
-    //     busuanzi.fetch()
-    //   }
-    // }
+    if (inBrowser) {
+      router.onAfterRouteChange = () => {
+        busuanzi.fetch()
+      }
+    }
   },
 }
 
